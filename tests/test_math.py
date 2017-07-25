86de03f2e5b80d95231f67ad57598f83f4617166
marissa <mle99958@uga.edu>
2017-07-25 15:38:17 -0400

    Add cool pytest test suite stuff

--- tests/test_math.py
+++ tests/test_math.py
@@ -9,11 +9,18 @@ def test_add():
     assert ot.math.add(5, 2) == 7
     assert ot.math.add(2, 5) == 7
 
-def test_mult():
-    assert ot.math.mult(1, 3) == 3
-    assert ot.math.mult(3, 1) == 3
-    assert ot.math.mult(2, 5) == 10
-    assert ot.math.mult(5, 2) == 10
+testdata = [
+    (2, 5, 10),
+    (1, 2, 2),
+    (11, 9, 99),
+    (11, 0, 0),
+    (0, 0, 0),
+]
+
+@pytest.mark.parametrize("a,b,expected", testdata)
+def test_mult(a, b, expected):
+    assert ot.math.mult(a, b) == expected
+    assert ot.math.mult(b, a) == expected
 
 #def test_awesome():
 
